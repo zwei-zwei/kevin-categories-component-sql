0497ce71d502c98b796369d660dc1f86e7cebf4f
George gd Fekaris <gdfekaris@gmail.com>
2019-01-06 01:31:13 -0800

    Working on API calls to database (using fake data)

--- server/server.js
+++ server/server.js
@@ -2,39 +2,49 @@ const express = require(`express`);
 const app = express();
 const bodyParser = require(`body-parser`);
 const db = require('../database');
-const faker = require('faker');
-const user = require('../database/seedScript')
 
 app.use(bodyParser.json());
 app.use(express.static(`${__dirname}/../public`));
 
-app.get('/categories', (req, res) => {
+app.get('/categories-module/users', (req, res) => {
   db.connection.query('SELECT * FROM users', (error, results, fields) => {
     if (error) {
-      console.log(error);
+      res.send(error);
     } else {
-      console.log(results);
+      console.log(JSON.parse(JSON.stringify(results)));
+      res.send(JSON.parse(JSON.stringify(results)));
     }
   });
-  res.end();
 });
 
-app.get(`/categories/getFromMain`, () => {
-  //fill me in
+app.get('/categories-module/games', (req, res) => {
+  db.connection.query('SELECT * FROM games', (error, results, fields) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(JSON.parse(JSON.stringify(results)));
+    }
+  });
 });
 
-app.post(`categories/writeToCats`, () => {
-  //fill me in
+app.get('/categories-module/videos', (req, res) => {
+  db.connection.query('SELECT * FROM videos', (error, results, fields) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(JSON.parse(JSON.stringify(results)));
+    }
+  });
 });
 
-app.put(`categories/writeToCats`, () => {
-  //fill me in
-});
-
-app.get('/fakes', (req, res) => {
-  let fakeUsername = faker.random.word() + faker.internet.userName();
-  let name = fakeUsername.replace(/ /, '');
-  res.send();
+app.get('/categories-module/clips', (req, res) => {
+  db.connection.query('SELECT * FROM clips', (error, results, fields) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(JSON.parse(JSON.stringify(results)));
+    }
+  });
 });
 
 // port assignment needs re-writing
