d9c8e451c79a5ff8966848266fc8ac4aff3e31a7
George gd Fekaris <gdfekaris@gmail.com>
2019-01-07 23:16:02 -0800

    rewriting seedScript

--- database/schema.sql
+++ database/schema.sql
@@ -3,41 +3,55 @@ CREATE DATABASE categories_module;
 USE categories_module;
 
 CREATE TABLE users (
-  id INT unsigned NOT NULL AUTO_INCREMENT,
-  display_name VARCHAR(150) NOT NULL,
+  streamer_id INT unsigned NOT NULL AUTO_INCREMENT,
+  display_name VARCHAR(100) NOT NULL,
   personal_description VARCHAR(500),
   profile_image_url VARCHAR(1000) NOT NULL,
   offline_image_url VARCHAR(1000),
   view_count INT,
-  PRIMARY KEY (id)
+  follower_count INT,
+  PRIMARY KEY (streamer_id)
 );
 
 CREATE TABLE games (
-  id INT unsigned NOT NULL AUTO_INCREMENT,
+  game_id INT unsigned NOT NULL AUTO_INCREMENT,
   title VARCHAR(150) NOT NULL,
   box_art_url VARCHAR(1000) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (game_id)
 );
 
 CREATE TABLE videos (
-  id INT unsigned NOT NULL AUTO_INCREMENT,
-  title VARCHAR(150) NOT NULL,
+  video_id INT unsigned NOT NULL AUTO_INCREMENT,
+  video_title VARCHAR(150) NOT NULL,
   video_description VARCHAR(500),
-  url VARCHAR(1000) NOT NULL,
-  thumbnail_url VARCHAR(1000) NOT NULL,
+  url VARCHAR(500) NOT NULL,
+  thumbnail_url VARCHAR(500) NOT NULL,
+  duration TIME, /* 'HH:MM:SS[.fraction]' */
   view_count INT,
-  PRIMARY KEY (id)
+  created_at DATE, /* 'YYYY-MM-DD' */
+  streamer_id INT unsigned NOT NULL,
+  game_id INT unsigned NOT NULL,
+  PRIMARY KEY (video_id),
+  FOREIGN KEY (streamer_id) REFERENCES users(streamer_id) ON DELETE CASCADE,
+  FOREIGN KEY (game_id) REFERENCES games(game_id) ON DELETE CASCADE
 );
 
 CREATE TABLE clips (
-  id INT unsigned NOT NULL AUTO_INCREMENT,
-  title VARCHAR(150) NOT NULL,
+  clip_id INT unsigned NOT NULL AUTO_INCREMENT,
+  clip_title VARCHAR(150) NOT NULL,
   clip_description VARCHAR(500),
-  url VARCHAR(1000) NOT NULL,
-  thumbnail_url VARCHAR(1000) NOT NULL,
-  duration INT,
+  url VARCHAR(500) NOT NULL,
+  thumbnail_url VARCHAR(500) NOT NULL,
+  duration TIME,
   view_count INT,
-  PRIMARY KEY (id)
+  created_at DATETIME,
+  streamer_id INT unsigned NOT NULL,
+  game_id INT unsigned NOT NULL,
+  video_id INT unsigned NOT NULL,
+  PRIMARY KEY (clip_id),
+  FOREIGN KEY (streamer_id) REFERENCES users(streamer_id) ON DELETE CASCADE,
+  FOREIGN KEY (game_id) REFERENCES games(game_id) ON DELETE CASCADE,
+  FOREIGN KEY (video_id) REFERENCES videos(video_id) ON DELETE CASCADE
 );
 
 
@@ -46,4 +60,8 @@ N.B.
 Execute this file from the command line by typing:
 mysql -u root < database/schema.sql
 to create the database and the tables.
+
+Then, to view the db and tables,
+login to mysql from command line by typing:
+mysql -u root -p
 */
--- database/seedScript.js
+++ database/seedScript.js
@@ -2,42 +2,233 @@ const faker = require('faker');
 const db = require('./index.js');
 
 module.exports = () => {
-  for (i = 0; i < 5; i++) {
+  const createUsersRow = new Promise(function (resolve, reject) {
     let user = `INSERT INTO users
-            (display_name, personal_description, profile_image_url, offline_image_url)
+            (display_name,
+            personal_description,
+            profile_image_url,
+            offline_image_url,
+            view_count,
+            follower_count)
             VALUES
-            ('${faker.internet.userName()}',
-            '${faker.random.words()}',
-            '${faker.internet.avatar()}',
-            '${faker.internet.avatar()}')`;
-    let game = `INSERT INTO games
-            (title, box_art_url)
-            VALUES
-            ('${faker.hacker.verb() + faker.hacker.noun()}',
-            '${faker.random.image()}')`;
-    let video = `INSERT INTO videos
-            (title, video_description, url, thumbnail_url)
-            VALUES
-            ('${faker.hacker.ingverb() + faker.hacker.noun()}',
-            '${faker.random.words()}',
-            '${faker.internet.url()}',
-            '${faker.random.image()}')`;
-    let clip = `INSERT INTO clips
-            (title, clip_description, url, thumbnail_url, duration)
-            VALUES
-            ('${faker.hacker.ingverb() + faker.hacker.adjective() + faker.hacker.noun()}',
-            '${faker.random.words()}',
-            '${faker.internet.url()}',
-            '${faker.random.image()}',
+            ('itzemario',
+            'Italian plumber. Strong and brave and funny. Once known as Jumpman',
+            'https://itunes.apple.com/us/app/super-mario-run/id1145275343?mt=8',
+            'http://www.zero-friction.net/wp-content/uploads/2015/08/mario_sleeping_by_dominiquepucca-d5vq698.png.jpg',
+            '${faker.random.number()}',
             '${faker.random.number()}')`;
-    db.connection.query(user);
-    db.connection.query(game);
-    db.connection.query(video);
-    db.connection.query(clip);
-  }
-  db.connection.end();
-  return 'database has been seeded';
+    db.connection.query(user, (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        resolve(console.log('users tabled seeded'));
+      }
+    });
+  });
+
+  const createGamesRow = new Promise(function (resolve, reject) {
+    for (i = 0; i < 50; i++) {
+      let game = `INSERT INTO games
+            (title,
+            box_art_url)
+            VALUES
+            ('${faker.name.jobDescriptor()}',
+            '${faker.random.image()}')`;
+      if (i === 49) {
+          db.connection.query(game, (err, res) => {
+            if (err) {
+              console.log(err);
+            } else {
+              resolve(console.log('games tabled seeded'));
+            }
+          });
+      } else {
+        db.connection.query(game, (err) => { if (err) { console.log(err) } });
+      }
+    }
+  });
+
+  const getUserIds = new Promise(function (resolve, reject) {
+    db.connection.query(`SELECT streamer_id FROM users`, (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        resolve(userIds = res);
+      }
+    });
+  });
+
+  const getGameIds = new Promise(function (resolve, reject) {
+    db.connection.query(`SELECT game_id FROM games`, (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        resolve(gameIds = res);
+      }
+    });
+  });
+
+  const createVideosRow = new Promise(function (resolve, reject) {
+    db.connection.query('SELECT * FROM users', (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        db.connection.end();
+        userIds = JSON.parse(JSON.stringify(userIds));
+        gameIds = JSON.parse(JSON.stringify(gameIds));
+        resolve(console.log(`USER IDS\n${userIds[0].streamer_id}\nGAME IDS\n${gameIds}`));
+      }
+    });
+  });
+
+  createUsersRow
+    .then(result => createGamesRow)
+    .then(result => getUserIds)
+    .then(result => getGameIds)
+    .then(result => createVideosRow);
+
+  return 'database seed script called';
 };
 
 require('make-runnable');
 
+
+// new Promise (function(resolve, reject) {
+  //   let user = `INSERT INTO users
+  //           (display_name,
+  //           personal_description,
+  //           profile_image_url,
+  //           offline_image_url,
+  //           view_count,
+  //           follower_count)
+  //           VALUES
+  //           ('itzemario',
+  //           'Italian plumber. Strong and brave and funny. Once known as Jumpman',
+  //           'https://itunes.apple.com/us/app/super-mario-run/id1145275343?mt=8',
+  //           'http://www.zero-friction.net/wp-content/uploads/2015/08/mario_sleeping_by_dominiquepucca-d5vq698.png.jpg',
+  //           '${faker.random.number()}',
+  //           '${faker.random.number()}')`;
+  //   db.connection.query(user, (err, res) => {
+  //     if (err) {
+  //       console.log(err);
+  //     } else {
+  //       resolve('users tabled seeded');
+  //     }
+  //   });
+  // }).then(function(result) {
+  //   console.log(result);
+  //   for (i = 0; i < 50; i++) {
+  //     let game = `INSERT INTO games
+  //           (title,
+  //           box_art_url)
+  //           VALUES
+  //           ('${faker.name.jobDescriptor()}',
+  //           '${faker.random.image()}')`;
+  //     if (i === 49) {
+  //       return new Promise (function(resolve, reject) {
+  //         db.connection.query(game, (err, res) => {
+  //           if (err) {
+  //             console.log(err);
+  //           } else {
+  //             resolve('games tabled seeded');
+  //           }
+  //         });
+  //       });
+  //     } else {
+  //       db.connection.query(game, (err) => {if (err) {console.log(err)}});
+  //     }
+  //   }
+  // }).then(function(result) {
+  //   console.log(result);
+  //   return new Promise (function(resolve, reject) {
+  //     db.connection.query(`SELECT streamer_id FROM users`, (err, res) => {
+  //       if (err) {
+  //         console.log(err);
+  //       } else {
+  //         resolve(JSON.parse(JSON.stringify(res)));
+  //       }
+  //     });
+  //   });
+  // }).then(function(result) {
+  //   let streamerIDs = result;
+  //   let idArray = [streamerIDs];
+  //   return new Promise (function(resolve, reject) {
+  //     db.connection.query(`SELECT game_id FROM games`, (err, res) => {
+  //       if (err) {
+  //         console.log(err);
+  //       } else {
+  //         let gameIDs = JSON.parse(JSON.stringify(res))
+  //         idArray.push(gameIDs);
+  //         resolve(idArray);
+  //       }
+  //     });
+  //   });
+  // }).then(function(result) {
+  //   db.connection.end();
+  //   console.log(result);
+  // });
+///////////////////////////////////////////
+
+// module.exports = () => {
+//   for (i = 0; i < 5; i++) {
+//     let user = `INSERT INTO users
+//             (display_name,
+//             personal_description,
+//             profile_image_url,
+//             offline_image_url,
+//             view_count,
+//             follower_count)
+//             VALUES
+//             ('itzemario',
+//             'Italian plumber. Strong and brave and funny. Once known as Jumpman',
+//             'https://itunes.apple.com/us/app/super-mario-run/id1145275343?mt=8',
+//             'http://www.zero-friction.net/wp-content/uploads/2015/08/mario_sleeping_by_dominiquepucca-d5vq698.png.jpg',
+//             '${faker.random.number()}',
+//             '${faker.random.number()}')`;
+//     let game = `INSERT INTO games
+//             (title,
+//             box_art_url)
+//             VALUES
+//             ('${faker.name.jobDescriptor()}',
+//             '${faker.random.image()}')`;
+//     let video = `INSERT INTO videos
+//             (video_title,
+//             video_description,
+//             url,
+//             thumbnail_url,
+//             duration,
+//             view_count,
+//             created_at)
+//             VALUES
+//             ('${faker.hacker.verb()/* video_title */}',
+//             '${faker.lorem.sentence()/* video_description */}',
+//             '${faker.internet.url()/* url */}',
+//             '${faker.random.image()/* thumbnail_url */}',
+//             '${faker.date.recent().substring(11, 19)/* duration */}:',
+//             '${faker.random.number()/* view_count */}',
+//             '${faker.date.recent().substring(0, 10)/* created_at */}')`;
+//     let clip = `INSERT INTO clips
+//             (clip_title,
+//             clip_description,
+//             url,
+//             thumbnail_url,
+//             duration,
+//             view_count,
+//             created_at)
+//             VALUES
+//             ('${faker.hacker.ingverb()/* clip_title */}',
+//             '${faker.lorem.sentence()/* clip_description */}',
+//             '${faker.internet.url()/* url */}',
+//             '${faker.random.image()/* thumbnail_url */}',
+//             '${faker.date.recent().substring(11, 19)/* duration */}:',
+//             '${faker.random.number()/* view_count */}',
+//             '${faker.date.recent().substring(0, 10)/* created_at */}')`;
+
+//     if (i === 0) { db.connection.query(user); }
+//     db.connection.query(game);
+//     db.connection.query(video);
+//     db.connection.query(clip);
+//   }
+//   db.connection.end();
+//   return 'database has been seeded';
+// };
