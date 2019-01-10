257893241f46bdc2e4d67c76bf14dba8953fb788
George gd Fekaris <gdfekaris@gmail.com>
2019-01-10 05:54:46 -0800

    working on carousel styling and functionality

--- client/components/Categorized_Lists.jsx
+++ client/components/Categorized_Lists.jsx
@@ -1,31 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import $ from 'jquery';
+import RecentBroadcasts from './RecentBroadcasts.jsx';
+import RecentHighlights from './RecentHighlights.jsx';
+import PopularClips from './PopularClips.jsx';
 
 export default class Categorized_Lists extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0
+      count: 0,
     }
-    this.count = this.count.bind(this);
+
   };
 
-  count = () => {
-    this.setState((prevState) => ({
-      count: prevState.count + 1,
-    }));
+  componentDidMount() {
+    fetch('http://localhost:1128/popular')
+    .then(response => response.json())
+    .then(data => this.setState({videos: data}));
   }
 
   render() {
     return (
       <div>
-        <span>Categorization Component</span>
-        <div>
-          <button onClick={this.count} data-testid="counter-button">
-            {this.state.count}
-          </button>
-        </div>
+        <RecentBroadcasts videos={this.state.videos} />
+        <RecentHighlights videos={this.state.videos} />
+        <PopularClips videos={this.state.videos}/>
       </div>
     );
   }
