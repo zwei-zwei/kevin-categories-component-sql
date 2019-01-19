b27347a35ea3ff13068891e674442162a4e8c305
George gd Fekaris <gdfekaris@gmail.com>
2019-01-19 15:34:43 -0800

    Working on deployment!

--- client/components/CarouselGenerator.jsx
+++ client/components/CarouselGenerator.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Carousel from 'nuka-carousel';
+import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import SlideGenerator from './SlideGenerator.jsx';
 import { Container, Titles, ExpandLink } from './Styles.jsx';
 import { PrevButton, NextButton } from './Buttons.jsx';
+import Videos from './Videos.jsx';
 
 export default class CarouselGenerator extends React.Component {
   render() {
@@ -11,29 +13,39 @@ export default class CarouselGenerator extends React.Component {
     });
 
     return (
-      <div>
-        <Container className="mainContainer">
-          <Titles>{this.props.title} <ExpandLink href={this.props.videos[0].url}>Expand All ></ExpandLink></Titles>
-          <Carousel
-            slidesToShow={2}
-            wrapAround={false}
-            cellSpacing={0}
-            initialSlideHeight={220}
-            initialSlideWidth={300}
-            renderBottomCenterControls={false}
-            framePadding={"22px"}
-            renderCenterLeftControls={({ previousSlide }) => (
-              <PrevButton onClick={previousSlide}>&#60;</PrevButton>
-            )}
-            renderCenterRightControls={({ nextSlide }) => (
-              <NextButton onClick={nextSlide}>></NextButton>
-            )}
-          >
-            {slides}
-          </Carousel>
-        </Container>
-      </div>
+      <Router>
+        <Switch>
+
+          <div>
+            <Container className="mainContainer">
+              <Titles>{this.props.title}
+                <ExpandLink>
+                  <Link to="/videosAll">Expand All ></Link>
+                </ExpandLink>
+              </Titles>
+              <Carousel
+                slidesToShow={2}
+                wrapAround={false}
+                cellSpacing={0}
+                initialSlideHeight={220}
+                initialSlideWidth={300}
+                renderBottomCenterControls={false}
+                framePadding={"22px"}
+                renderCenterLeftControls={({ previousSlide }) => (
+                  <PrevButton onClick={previousSlide}>&#60;</PrevButton>
+                )}
+                renderCenterRightControls={({ nextSlide }) => (
+                  <NextButton onClick={nextSlide}>></NextButton>
+                )}
+              >
+                {slides}
+              </Carousel>
+            </Container>
+          </div>
+
+          <Route path="/videosAll" component={Videos} />
+        </Switch>
+      </Router>
     );
   };
 };
-
--- client/components/Categorized_Lists.jsx
+++ client/components/Categorized_Lists.jsx
@@ -3,6 +3,7 @@ import RecentBroadcasts from './RecentBroadcasts.jsx';
 import RecentHighlights from './RecentHighlights.jsx';
 import PopularClips from './PopularClips.jsx';
 import AllVideos from './AllVideos.jsx';
+import { Switch, HashRouter, Route } from 'react-router-dom';
 
 export default class Categorized_Lists extends React.Component {
   constructor(props) {
@@ -18,30 +19,30 @@ export default class Categorized_Lists extends React.Component {
   componentDidMount() {
     let allVideos = {};
     fetch('http://localhost:1128/recent-broadcasts')
-    .then(response => response.json())
-    .then((data) => {
-      allVideos.recentBroadcasts = data;
-      return fetch('http://localhost:1128/recent-highlights');
-    })
-    .then(response => response.json())
-    .then((data) => {
-      allVideos.recentHighlights = data;
-      return fetch('http://localhost:1128/popular-clips');
-    })
-    .then(response => response.json())
-    .then((data) => {
-      allVideos.popularClips = data;
-      return allVideos;
-    })
-    .then((allVideos) => {
-      let all = allVideos.recentBroadcasts.concat(allVideos.recentHighlights, allVideos.popularClips);
-      this.setState({
-        allVideos: all,
-        recentBroadcasts: allVideos.recentBroadcasts,
-        recentHighlights: allVideos.recentHighlights,
-        popularClips: allVideos.popularClips
+      .then(response => response.json())
+      .then((data) => {
+        allVideos.recentBroadcasts = data;
+        return fetch('http://localhost:1128/recent-highlights');
+      })
+      .then(response => response.json())
+      .then((data) => {
+        allVideos.recentHighlights = data;
+        return fetch('http://localhost:1128/popular-clips');
+      })
+      .then(response => response.json())
+      .then((data) => {
+        allVideos.popularClips = data;
+        return allVideos;
+      })
+      .then((allVideos) => {
+        let all = allVideos.recentBroadcasts.concat(allVideos.recentHighlights, allVideos.popularClips);
+        this.setState({
+          allVideos: all,
+          recentBroadcasts: allVideos.recentBroadcasts,
+          recentHighlights: allVideos.recentHighlights,
+          popularClips: allVideos.popularClips
+        });
       });
-    });
   };
 
   render() {
@@ -49,12 +50,26 @@ export default class Categorized_Lists extends React.Component {
       return (<div data-testid="loading-div">Loading ...</div>);
     } else {
       return (
-        <div data-testid="main-container">
-          <RecentBroadcasts videos={this.state.recentBroadcasts} />
-          <RecentHighlights videos={this.state.recentHighlights} />
-          <PopularClips videos={this.state.popularClips} />
-          <AllVideos videos={this.state.allVideos} />
-        </div>
+        <HashRouter>
+          <Switch>
+            <Route exact={true} path="/" render={() => (
+              <div data-testid="main-container">
+                <RecentBroadcasts videos={this.state.recentBroadcasts} />
+                <RecentHighlights videos={this.state.recentHighlights} />
+                <PopularClips videos={this.state.popularClips} />
+                <AllVideos videos={this.state.allVideos} />
+              </div>
+            )} />
+            <Route exact={true} path="/videos" render={() => (
+              <div data-testid="main-container">
+                <RecentBroadcasts videos={this.state.recentBroadcasts} />
+                <RecentHighlights videos={this.state.recentHighlights} />
+                <PopularClips videos={this.state.popularClips} />
+                <AllVideos videos={this.state.allVideos} />
+              </div>
+            )} />
+          </Switch>
+        </HashRouter>
       );
     }
   };
