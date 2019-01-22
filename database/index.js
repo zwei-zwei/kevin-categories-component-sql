d358f8fd7a64d8915e35e282ab13c43f3e3ce3e8
George gd Fekaris <gdfekaris@gmail.com>
2019-01-21 23:41:15 -0800

    using dynamic version (master branch) to generate data for static version (static branch)

--- database/index.js
+++ database/index.js
@@ -1,25 +1,30 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'toor',
-  database: 'categories_module'
-
-
-  // host: process.env.RDS_HOSTNAME,
-  // user: process.env.RDS_USERNAME,
-  // password: process.env.RDS_PASSWORD,
-  // port: process.env.RDS_PORT,
+  // host: 'localhost',
+  // user: 'root',
+  // password: 'toor',
   // database: 'categories_module'
+
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT,
+  database: 'categories_module'
 });
 
 connection.connect((err) => {
   if (err) {
-    process.env['msg'] = 'Unable to connect to RDS - ' + err;/* console.log(`ERROR: ${err.message}`); */
+    console.log(`ERROR: ${err.message}`);
   } else {
-    process.env['msg'] = 'Success! Connected to RDS via ' + process.env.RDS_HOSTNAME;/* console.log('database connected'); */
+    console.log('database connected');
   }
+
+  // if (err) {
+  //   process.env['msg'] = 'Unable to connect to RDS - ' + err;
+  // } else {
+  //   process.env['msg'] = 'Success! Connected to RDS via ' + process.env.RDS_HOSTNAME;
+  // }
 });
 
 module.exports.connection = connection;
