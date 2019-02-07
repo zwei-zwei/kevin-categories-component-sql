e8e840b81e29f8e4179c374433c69d558fa023a6
Kevin Haiduy Phung <kphung29@gmail.com>
2019-02-07 17:47:00 -0500

    setting up POST route to test

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(`${__dirname}/../public`));
 
+//GET requests
 app.get('/recent-broadcasts', (req, res) => {
   Video.findAll({
     limit: 100,
@@ -29,6 +30,17 @@ app.get('/recent-highlights', (req, res) => {
       res.send(data);
     })
     .catch(err => console.log(err));
+});
+
+//POST requests
+app.post('/add-video', (req, res) => {
+  if (!req.body) {
+    console.log('Not Found');
+  } else {
+    Video.create()
+    .then(video => res.sendStatus(201).json(video))
+    .catch(err => console.log(err));
+  }
 });
 
 const port = process.env.PORT || 3000;
