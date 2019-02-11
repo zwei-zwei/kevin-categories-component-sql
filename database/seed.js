d44388154a51a4ea834a9cc6c3c628700e0c4722
Kevin Haiduy Phung <kphung29@gmail.com>
2019-02-11 18:50:03 -0500

    10milly to ec2 instance, prepare to stress test routes

--- database/seed.js
+++ database/seed.js
@@ -1,4 +1,4 @@
-const Video = require('../database/index');
+const Video = require('./indexSequelize');
 const faker = require('faker');
 require('events').EventEmitter.prototype._maxListeners = 1000;
 
@@ -9,7 +9,6 @@ async function seedData(outer, inner) {
     let arr = [];
     for (let j = 0; j < inner; j++) {
       let obj = {
-        // video_id: faker.random.number(),
         user_name: faker.internet.userName(),
         game_name: faker.commerce.productName(),
         game_box_art_url: faker.image.imageUrl(),
