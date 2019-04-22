f86029396a4b314859103bd6fc17856bfbb2fdf4
Kevin Haiduy Phung <kphung29@gmail.com>
2019-04-22 18:38:37 -0400

    redeploy updated config

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  postGresURI: 'ec2-3-84-112-211.compute-1.amazonaws.com',
+  postGresURI: 'ec2-54-159-208-155.compute-1.amazonaws.com',
   postGresDB: 'twitchec2',
   postGresUser: 'postgres',
   postGresPassWord: 'postgres'
